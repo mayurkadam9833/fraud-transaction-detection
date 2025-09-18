7a85f59686a54472bf9a0583c13a086eb80d421e
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-09-18 22:51:54 +0530

    define yaml files path

---
+++ src/fraud_transaction_detection/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path 
+
+CONFIG_FILE_PATH=Path("config/config.yaml")  # config.yaml file path
+SCHEMA_FILE_PATH=Path("schema.yaml")         # schema.yaml file path
+PARAMS_FILE_PATH=Path("params.yaml")         # params.yaml file path
